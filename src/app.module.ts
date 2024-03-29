c9da1bf6b8b31479378883a8ea86102b611f5085
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-03-29 21:32:16 +0100

    schema and model creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,16 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
   imports: [
+    //Con este modulo podemos correr  el servidor de archivos estaticos, es decir los que no son gestionados por nest
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule
   ],
   controllers: [],
--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,21 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+//Al extender de document de mongoose nuestra entidad este le añadira toda la funcionalidad o informacion respectiva a nuestra entidad
+@Schema()
+export class Pokemon extends Document {
+    
+    @Prop({
+        unique:true,
+        index: true,
+    })
+    name: string;
+
+    @Prop({
+        unique:true,
+        index: true,
+    })
+    no: number
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
