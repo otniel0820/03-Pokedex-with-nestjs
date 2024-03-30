88ae46b5fb8b3fb1e82abcfdc8857a289290d322
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-03-30 10:03:46 +0100

    http request

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 
 @Injectable()
 export class SeedService {
   
+  private readonly axios: AxiosInstance = axios
 
-  executeSeed() {
-    return `Seed execute`;
+  async executeSeed() {
+    const { data }= await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+
+    data.results.forEach(async ({name, url}) => {
+      
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+      console.log({name, no});
+      
+      
+    })
+
+
+    return data.results;
   }
 
  
